be10347d65de6701ef26ede812fe29f31e59627f
luminostar <rafialayyubi08@gmail.com>
2023-11-24 09:43:22 +0800

    little change oneVar and twoVar

--- src/PSO/oneVar.cpp
+++ src/PSO/oneVar.cpp
@@ -85,7 +85,6 @@ public:
             cout << val << " ";
         }
         cout << endl << "gBest = " << gBest << endl << endl;
-        cout << endl;
 
         for (int i = 0; i < n; ++i) {
             cout << "Iterasi ke-" << i + 1 << endl;
@@ -124,7 +123,7 @@ int main() {
     double w = 1;
 
     PSO pso(x, v, c, r, w);
-    pso.iterate(3);
+    pso.iterate(50);
 
     return 0;
 }
